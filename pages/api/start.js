77c5638b195953b6c9b8ba80f899251c0bc7001d
scriptifid <scriptifid@gmail.com>
2025-05-05 20:51:11 -0500

    Aggreed

--- pages/api/start.js
+++
@@ -1,7 +0,0 @@
-let lobbies = global.lobbies || (global.lobbies = {})
-
-export default function handler(req, res) {
-  const { code } = req.body
-  if (lobbies[code]) lobbies[code].started = true
-  res.json({ success: true })
-}
--- pages/api/status.js
+++
@@ -1,8 +0,0 @@
-let lobbies = global.lobbies || (global.lobbies = {})
-
-export default function handler(req, res) {
-  const { code } = req.query
-  const lobby = lobbies[code]
-  if (!lobby) return res.status(404).json({ success: false })
-  res.json({ players: lobby.players, started: lobby.started })
-}
